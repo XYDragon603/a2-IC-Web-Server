60c2a10f7b81efdf140560ac8dba88c059e91c97
YunLong Li <yunlongli518@gmail.com>
2026-03-12 11:32:02 +0700

    Update

--- projects/p2/src/parse.h
+++ projects/p2/src/parse.h
@@ -1,30 +1,30 @@
-#include <string.h>
+#ifndef PARSE_H
+#define PARSE_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SUCCESS 0
 
-//Header field
-typedef struct
-{
-	char header_name[4096];
-	char header_value[4096];
+typedef struct {
+    char header_name[4096];
+    char header_value[4096];
 } Request_header;
 
-//HTTP Request Header
-typedef struct
-{
-	char http_version[50];
-	char http_method[50];
-	char http_uri[4096];
-	Request_header *headers;
-	int header_count;
+typedef struct {
+    char http_version[50];
+    char http_method[50];
+    char http_uri[4096];
+    Request_header *headers;
+    int header_count;
 } Request;
 
-Request* parse(char *buffer, int size,int socketFd);
+Request *parse(char *buffer, int size, int socketFd);
+void free_request(Request *request);
 
-// functions decalred in parser.y
-int yyparse();
+int yyparse(void);
 void set_parsing_options(char *buf, size_t i, Request *request);
-// to allow resetting the parser the request failed to properly parse
-void yyrestart(FILE *input_file); 
+void yyrestart(FILE *input_file);
+
+#endif
--- projects/p2/src/sample_parse.c
+++ projects/p2/src/sample_parse.c
@@ -1,33 +1,52 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include "parse.h"
 
-int main(int argc, char **argv){
-  //Read from the file the sample
-  int fd_in = open(argv[1], O_RDONLY);
-  int index;
-  char buf[8192];
-	if(fd_in < 0) {
-		printf("Failed to open the file\n");
-		return 0;
-	}
-  int readRet = read(fd_in,buf,8192);
-  //Parse the buffer to the parse function. You will need to pass the socket fd and the buffer would need to
-  //be read from that fd
-  Request *request = parse(buf,readRet,fd_in);
-  //Just printing everything
-  printf("Http Method %s\n",request->http_method);
-  printf("Http Version %s\n",request->http_version);
-  printf("Http Uri %s\n",request->http_uri);
-  for(index = 0;index < request->header_count;index++){
-    printf("Request Header\n");
-    printf("Header name %s Header Value %s\n",request->headers[index].header_name,request->headers[index].header_value);
-  }
-  free(request->headers);
-  free(request);
-  return 0;
+int main(int argc, char **argv) {
+    int fd_in;
+    int index;
+    int read_ret;
+    char buf[8192];
+    Request *request;
+
+    if (argc < 2) {
+        printf("usage: %s <request-file>\n", argv[0]);
+        return 1;
+    }
+
+    fd_in = open(argv[1], O_RDONLY);
+    if (fd_in < 0) {
+        printf("Failed to open the file\n");
+        return 1;
+    }
+
+    read_ret = read(fd_in, buf, sizeof(buf));
+    close(fd_in);
+
+    if (read_ret <= 0) {
+        printf("Failed to read the file\n");
+        return 1;
+    }
+
+    request = parse(buf, read_ret, -1);
+    if (request == NULL) {
+        printf("Parse failed\n");
+        return 1;
+    }
+
+    printf("Http Method %s\n", request->http_method);
+    printf("Http Version %s\n", request->http_version);
+    printf("Http Uri %s\n", request->http_uri);
+
+    for (index = 0; index < request->header_count; index++) {
+        printf("Request Header\n");
+        printf("Header name %s Header Value %s\n",
+               request->headers[index].header_name,
+               request->headers[index].header_value);
+    }
+
+    free_request(request);
+    return 0;
 }
